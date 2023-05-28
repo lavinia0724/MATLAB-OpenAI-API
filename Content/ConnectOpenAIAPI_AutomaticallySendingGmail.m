4c2e80e9391294103abd1e31f45dbfe25bd83ab8
unknown <LAVI@nisra.net>
2023-05-29 00:56:56 +0800

    upload

--- Content/ConnectOpenAIAPI_AutomaticallySendingGmail.m
+++ Content/ConnectOpenAIAPI_AutomaticallySendingGmail.m
@@ -1,3 +1,23 @@
+import matlab.net.*
+import matlab.net.http.*
+
+% Connect OpenAI API
+api_endpoint = "https://api.openai.com/v1/engines/davinci/completions";
+api_key = convertCharsToStrings(getenv("APIkey"));
+prompt = "Please introduce Cloud Computing"
+
+parameters = struct('prompt',prompt, 'max_tokens',100);
+headers = matlab.net.http.HeaderField('Content-Type', 'application/json');
+headers(2) = matlab.net.http.HeaderField('Authorization', ['Bearer ' + api_key]);
+request = matlab.net.http.RequestMessage('post',headers,parameters);
+response = send(request, URI(api_endpoint));
+response_text = response.Body.Data;
+response_text = response_text.choices(1).text;
+disp(response_text);
+
+% --------------------------------------------------------------------------------------
+  
+% Automatically Sending Gmail
 % <source>@gmail.com 換成你的信箱
 % username = '<source user name>';
 % password = convertCharsToStrings(getenv("Passwd"));
